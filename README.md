753b453971930b5bc09b7b96e6b9b29c1a811449
PatchesTheWright <jonathan.valbe.jb@gmail.com>
2023-01-14 08:01:43 -0300

    Aula finalizada

---
+++ README.md
@@ -0,0 +1,5 @@
+# Índice das Aulas React Avançado
+📄 Link de acesso aos <a href="https://cord-delivery-7eb.notion.site/React-Avan-ado-0dd7bebfaf364c1f8544098923b060e5">resumos</a>. 
+
+🖼 Link de <a href="https://inquisitive-haupia-8cc939.netlify.app/">demonstração</a>.
+
---
+++ src/deck-of-cards/deck-of-cards.js
@@ -0,0 +1,50 @@
+import { Component, useEffect, useState } from "react";
+
+async function createDeck(){
+    const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    const deck = await response.json()
+    return deck.deck_id
+}
+
+async function getCards(deckId){
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
+    return await response.json()
+}
+
+const DeckOfCards = () => {
+
+    const [deck, setDeck] = useState({
+        cards: []
+    })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const deckId = await createDeck()
+            const data = await getCards(deckId)
+
+            setDeck({
+                cards: data.cards
+            })
+        }
+        fetchData()
+    }, [])
+
+    return (
+        <section>
+            <ul>
+                {
+                    deck.cards.map((card, index) => {
+                        return (
+                            <li key={index}>
+                                <img src={card.image} alt={card.value} />
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
+    )
+
+}
+
+export default DeckOfCards
